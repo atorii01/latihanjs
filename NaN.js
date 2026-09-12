e10cd0a4a15172da14896073808a8c380b0cff86
Peter <162819966+atorii01@users.noreply.github.com>
2026-09-12 14:42:02 +0700

    Create NaN.js

---
+++ NaN.js
@@ -0,0 +1,17 @@
+console.log('Java'/'Script');
+console.log('Java'-'Script');
+console.log('Java'+'Script');
+console.log('Java'*'Script');
+
+let a;
+console.log(a);
+
+let umur = "17 adalah umur saya";
+let angka = parseInt(umur);
+console.log(angka + 2)
+
+let c = Math.cos(3.14);
+console.log(c)
+
+let d;
+console.log(d)
